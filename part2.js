fc0b6c88b9d914bb7a60d8171b5c4cb96428628a
dmitriy365377 <tigr365377@gmail.com>
2020-09-15 23:21:21 +0300

    second commit

--- part2.js
+++ part2.js
@@ -1,3 +1,23 @@
 const R = require('ramda');
 
 
+// const average = R.lift(R.divide)(R.sum, R.length);
+// console.log(average([8, 6, 7, 5, 3])); //=> 5.8
+
+
+
+// const x = R.adjust(R.subtract(R.__, 10), 1, [100, 200, 300])
+// console.log(x)
+
+
+const products = [
+    { name: 'Jeans', price: 80, category: 'clothes', stock: 100 },
+    { name: 'Andrey', price: 50, category: 'car', stock: 22 },
+    { name: 'Dima', price: 100, category: 'bom-bom', stock: 33 }
+]
+
+const predicate = R.T
+
+const getResults = R.pipe(R.filter(predicate), R.pluck('name'))
+const result = getResults(products)
+console.log(result)
